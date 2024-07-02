a2526e8e4ae45f0e1fa7867939dad0f19d18f688
Flix <flixgit@outlook.com>
2024-07-02 13:22:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = 3000; // Choose any available port
+const port = 9000; // Choose any available port
 
 // Serve static files from the 'public' folder
 app.use(express.static(__dirname + '/public'));
